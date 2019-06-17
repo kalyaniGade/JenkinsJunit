c275fef223151ae49f5a3c4e8cd84dc26962ffa3
C-PN15 <C-PN15@DAL-8CO097-9W7.wks.tsh.thomson.com>
2019-06-17 19:54:38 +0530

    from eclispe

--- src/main/java/com/virtusa/jenkins/App.java
+++ src/main/java/com/virtusa/jenkins/App.java
@@ -10,5 +10,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+        Junit junit=new Junit();
+    	junit.add(junit.a, junit.b);
     }
 }
