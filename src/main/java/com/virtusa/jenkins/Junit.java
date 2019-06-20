273b7486bece6feeac794554524767ee3661e8e2
C-PN15 <C-PN15@DAL-8CO097-9W7.wks.tsh.thomson.com>
2019-06-20 12:22:13 +0530

    test case commit

--- src/main/java/com/virtusa/jenkins/Junit.java
+++ src/main/java/com/virtusa/jenkins/Junit.java
@@ -2,7 +2,7 @@ package com.virtusa.jenkins;
 
 public class Junit {
 	int a=10;
-	int b=16;
+	int b=10;
 	int add(int a,int b) {
 		return a+b;
 	}
--- src/test/java/com/virtusa/jenkins/JunitTest.java
+++ src/test/java/com/virtusa/jenkins/JunitTest.java
@@ -3,13 +3,23 @@ package com.virtusa.jenkins;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 public class JunitTest {
 
+	Junit junit;
+	
+	@BeforeEach
+	public void init() {
+		// junit=new Junit();
+		 System.out.println("Creating Instance");
+	}
+	
+	
 	@Test
 	public void test() {
-		Junit junit=new Junit();
-		assertEquals(26,junit.add(junit.a, junit.b));
+		 junit=new Junit();
+		assertEquals(20,junit.add(junit.a, junit.b));
 	}
 
 }
