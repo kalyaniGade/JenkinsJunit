f132e38acea1376718f8d937ba7e54bcde139582
C-PN15 <C-PN15@DAL-8CO097-9W7.wks.tsh.thomson.com>
2019-06-25 11:36:51 +0530

    Commit

--- src/test/java/com/virtusa/jenkins/JunitTest.java
+++ src/test/java/com/virtusa/jenkins/JunitTest.java
@@ -3,38 +3,37 @@ package com.virtusa.jenkins;
 import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 
 public class JunitTest {
 
 	Junit junit;
 	
-	@BeforeEach
+	@Before
 	public void init() {
 	   junit=new Junit();
 		 System.out.println("Creating Instance");
 	}
 	
-	@AfterEach
-	void cleanUp() {
+	@After
+	public void cleanUp() {
 		System.out.println("Cleaning Up...");
 	}
 	
 	@Test
-	@DisplayName("Testing  Add method")
 	public void test() {
-		 junit=new Junit();
+		
 		assertEquals(20,junit.add(junit.a, junit.b));
 	}
+	
+	@Ignore
 	@Test
-	@DisplayName("Testing  Add method")
 	public void test1() {
-		 junit=new Junit();
+		
 		assertEquals(20,junit.add(junit.a, junit.b));
 	}
 	
